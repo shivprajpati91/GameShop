d948ff8db2904ab3540d47e8ebae44387046c7d9
shiv prajapati <shivprajapati3435@gmail.com>
2025-01-08 15:25:38 +0530

    gameUI

---
+++ android/app/src/main/kotlin/com/example/gameshop/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.gameshop
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/screen/detail/image_size.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+
+import '../../constants.dart';
+
+class ImageSize extends StatelessWidget {
+  const ImageSize({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal:   10, vertical: 16),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Container(
+            height: 90,
+            width: 90,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: kCardBackgroundColor,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(
+                color: kSelectCardBackgroundColor,
+                width: 1.5,
+              ),
+            ),
+            child: Image.asset("assets/images/detail1.png"),
+          ),
+          Container(
+              height: 90,
+            width: 90,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: kCardBackgroundColor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Image.asset("assets/images/detail2.png"),
+          ),
+          Container(
+             height: 90,
+            width: 90,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: kCardBackgroundColor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Image.asset("assets/images/detail3.png"),
+          ),
+          Container(
+             height: 90,
+            width: 90,
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: kCardBackgroundColor,
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Image.asset("assets/images/detail4.png"),
+          ),
+        ],
+      ),
+    );
+  }
+}
